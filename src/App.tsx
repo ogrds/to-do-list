4829586c6975c38d7035a64eaeebef39726b9980
Gabriel Ribeiro <gabrielrsilva98@gmail.com>
2021-03-16 19:31:02 -0300

    Commit Inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
+import tasks from "./itens.json";
+
+interface Tasks {
+  id: number;
+  task: string;
+  status: boolean;
+}
 
 function App() {
+  const [finishedTasks, setFinishedTasks] = useState<Tasks[]>(tasks);
+
+  const endTask = (i: number) => {
+    const newTasks = finishedTasks.map(({ id, task, status }, key) => {
+      if (key === i) {
+        return {
+          id,
+          task,
+          status: true,
+        };
+      }
+      return {
+        id,
+        task,
+        status,
+      };
+    });
+
+    setFinishedTasks(newTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>To do List</h1>
       </header>
+      <main>
+        <section>
+          <span>A realizar:</span>
+          <ul>
+            {finishedTasks.map(({ id, task, status }, i) => {
+              if (!status) {
+                return (
+                  <li key={id}>
+                    <label htmlFor={id.toString()}>
+                      <input
+                        onChange={() => endTask(i)}
+                        type="checkbox"
+                        id={id.toString()}
+                        disabled={status}
+                      />
+                      {task}
+                    </label>
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        </section>
+        <div className="divider"></div>
+        <section>
+          <span>Concluídas:</span>
+          <ul>
+            {finishedTasks.map(({ id, task, status }, i) => {
+              if (status) {
+                return (
+                  <li key={id} className={status ? "taskFinished" : ""}>
+                    <label htmlFor={id.toString()}>
+                      <input
+                        type="checkbox"
+                        disabled={true}
+                      />
+                      {task}
+                    </label>
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        </section>
+      </main>
+      <footer></footer>
     </div>
   );
 }
